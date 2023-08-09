ce14eeb82ece81072d3769b316e156c654c159dc
Random Workings <99320484+RandomWorkings@users.noreply.github.com>
2023-08-09 22:30:49 +0100

    Refactoring Fetch Data

---
+++ src/Pages/FetchData.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+
+namespace RandomWorkings.github.io.Pages;
+
+partial class FetchData
+{
+    private PathwayVideos[]? _videos;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _videos = await Http.GetFromJsonAsync<PathwayVideos[]>( "sample-data/videoCount.json" );
+    }
+
+    public class PathwayVideos
+    {
+        public string? Pathway { get; set; }
+
+        public int VideoCount { get; set; }
+
+        public Uri? Link { get; set; }
+    }
+}
