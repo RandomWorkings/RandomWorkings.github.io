35cb4e42a2113e0d7c8523deb726ea75817100fc
Random Workings <99320484+RandomWorkings@users.noreply.github.com>
2023-08-09 20:05:19 +0100

    Refactor Files

---
+++ src/Components/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace RandomWorkings.github.io.Components;
+
+partial class NavMenu
+{
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        _collapseNavMenu = !_collapseNavMenu;
+    }
+}
