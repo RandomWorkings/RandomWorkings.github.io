441a8959efaa5d43c2a24c96759c8601a56a8c61
Random Workings <99320484+RandomWorkings@users.noreply.github.com>
2023-10-08 01:44:50 +0100

    Users/lizlarkman/001 fungrounds (#38)

---
+++ src/Components/Shared/JumpToTopButton.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace RandomWorkings.github.io.Components.Shared;
+
+partial class JumpToTopButton : Button
+{
+    [Parameter]
+    public string Src { get; set; } = string.Empty;
+
+    [Parameter] 
+    public string Width { get; set; } = "100%";
+
+    [Parameter]
+    public string Height { get; set; } = "100%";
+
+    [Parameter]
+    public string AlternativeText { get; set; } = "Unhelpful Alternative Text for an Image";
+
+    [Parameter]
+    public string LazyLoading { get; set; } = "lazy";
+    
+    public Dictionary<string, object> ImageAttributes { get => GenerateImageAttributes(); }
+
+    public Dictionary<string, object> GenerateImageAttributes()
+    {
+        var attributes = new Dictionary<string, object>
+        {
+            { "class", $"image {Classes}" },
+            { "src", Src },
+            { "width", Width },
+            { "height", Height },
+            { "alt",  AlternativeText },
+            { "loading", LazyLoading },
+        };
+
+        return attributes;
+    }
+
+    private async void OnButtonClick()
+    {
+        await JsRuntime.InvokeVoidAsync("OnScrollEvent");
+    }
+}
