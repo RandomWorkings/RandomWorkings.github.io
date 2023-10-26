a935e64325dafa1e2c87ebee821640cb84f747ab
Random Workings <99320484+RandomWorkings@users.noreply.github.com>
2023-10-26 02:47:22 +0100

    Users/lizlarkman/001 fungrounds (#50)

---
+++ src/Components/Shared/PageHeader.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RandomWorkings.github.io.Components.Shared;
+
+partial class PageHeader
+{
+    [Parameter]
+    public string Text { get; set; } = "Page Header";
+
+    [Parameter]
+    public string ImageSource { get; set; } = string.Empty;
+
+}
---
+++ src/Components/Shared/TabControl.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RandomWorkings.github.io.Components.Shared;
+
+partial class TabControl
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    public TabPage? ActivePage { get; set; }
+
+    public List<TabPage> Pages = new();
+
+    public void AddPage( TabPage tabPage )
+    {
+        Pages.Add( tabPage );
+        if ( Pages.Count == 1 )
+            ActivePage = tabPage;
+        StateHasChanged();
+    }
+
+    public void ActivatePage( TabPage page )
+    {
+        ActivePage = page;
+    }
+}
